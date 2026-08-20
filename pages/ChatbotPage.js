4a21b1492a4478d7da31e2ff9e08faaf8a2b13da
Sameena10 <ksameena143@gmail.com>
2026-08-20 10:50:34 +0530

    WORKSP-31

--- pages/ChatbotPage.js
+++ pages/ChatbotPage.js
@@ -8,6 +8,8 @@ class Chatbot{
             page.locator('#lm-chat-button');
 
 console.log("test 123 multiple commit")
+console.log("test 123 multiple commit")
+
 
         this.messageInput =
             page.getByRole(
@@ -44,6 +46,8 @@ console.log("test 123 multiple commit")
         await this.messageInput.waitFor({
             state: 'visible'
         });
+        console.log("test 123 multiple commit")
+
 
         await this.messageInput.click();
         await this.messageInput.focus();
@@ -87,6 +91,8 @@ console.log("test 123 multiple commit")
                 timeout: 120000
             }
         );
+console.log("test 123 multiple commit")
+console.log("test 123 multiple commit")
 
         // Wait until response becomes stable
         let previousText = '';
